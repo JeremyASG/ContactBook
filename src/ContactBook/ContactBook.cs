22de0b0a26886be1ddb561788846af33082c485f
JeremyASG <elhombrexdxd@gmail.com>
2026-04-30 16:32:15 -0400

    Add: Deduplicate + Merge Contacts

--- src/ContactBook/ContactBook.cs
+++ src/ContactBook/ContactBook.cs
@@ -455,10 +455,89 @@ public class ContactBook
 		filteredContacts.Sort(ccp);
 	}
 
-    private void DeduplicateContacts()
-    {
-        Console.WriteLine("Deduplicate Contacts");
-    }
+   private void DeduplicateContacts()
+	{
+		List<List<Contact>> duplicateGroups = ContactMerger.FindDuplicates(allContacts);
+
+		List<Contact> temp = new List<Contact>();
+
+		foreach(var group in duplicateGroups)
+		{
+			if(group.Count > 1)
+			{
+				Console.Clear();
+				Console.WriteLine(new string('#', 80));
+
+				Console.WriteLine("Duplicate Contacts");
+
+				Console.WriteLine(new string('#', 80));
+
+				Console.WriteLine();
+				ShowContacts(group, 1, group.Count);
+				int fnameIndex = GetInt("Enter first name index", 1, group.Count) - 1;
+
+				int lnameIndex = GetInt("Enter last name index", 1, group.Count) - 1;
+
+				int phoneIndex = GetInt("Enter phone index", 1, group.Count) - 1;
+
+				int emailIndex = GetInt("Enter email index", 1, group.Count) - 1;
+
+				Contact merged = new Contact();
+				merged.SetFName(group[fnameIndex].GetFName());
+
+				merged.SetLName(group[lnameIndex].GetLName());
+				merged.SetPhone(group[phoneIndex].GetPhone());
+
+				merged.SetEmail(group[emailIndex].GetEmail());
+
+				Console.WriteLine();
+				Console.WriteLine(new string('#', 80));
+
+				Console.WriteLine("Merged Contact");
+				Console.WriteLine(new string('#', 80));
+
+				Console.WriteLine();
+				
+				List<Contact> mergedLS = new List<Contact>{ merged };
+                
+				ShowContacts(mergedLS, 1, 1);
+				Console.WriteLine();
+
+				if(Confirm("Do you want to merge these contacts?", NO))
+				{
+					temp.Add(merged);
+					Console.WriteLine("Operation successful: Contacts merged.");
+				}
+				else
+				{
+					temp.AddRange(group);
+					Console.WriteLine("Operation cancelled: Contacts not merged.");
+				}
+
+				Console.WriteLine();
+				PressEnterContinue();
+			}
+			else
+			{
+				temp.AddRange(group);
+			}
+		}
+
+		Console.WriteLine();
+
+		if(Confirm("Do you want to apply all merges to the contact list?", NO))
+		{
+			allContacts = filteredContacts = temp;
+			Console.WriteLine("Operation successful: Contacts deduplicated.");
+		}
+		else
+		{
+			Console.WriteLine("Operation cancelled: Contacts not deduplicated.");
+		}
+
+		Console.WriteLine();
+		PressEnterContinue();
+	}
 
     private void Exit()
     {
