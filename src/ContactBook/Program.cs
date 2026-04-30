f7db9b08ac214853e544c93c216a7fec9d4af85c
JeremyASG <elhombrexdxd@gmail.com>
2026-04-30 14:14:03 -0400

    Add: Contact class

--- src/ContactBook/Program.cs
+++ src/ContactBook/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace ContactBook;
+
+public class Program
+{
+	public static void Main()
+	{
+		Contact c1 = new Contact();
+        Contact c2 = new Contact("Jane", "Smith", "0987654321");
+        Contact c3 = new Contact("John", "Doe", "1234567890", "john.doe@example.com");
+	}
+}
