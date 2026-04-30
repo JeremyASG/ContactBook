c6050995948085450b46424b78b117a45f3e9667
JeremyASG <elhombrexdxd@gmail.com>
2026-04-30 15:36:27 -0400

    Add: ShowContacts

--- src/ContactBook/ContactBook.cs
+++ src/ContactBook/ContactBook.cs
@@ -161,7 +161,16 @@ public class ContactBook
 
         private bool IsValidInput(string input)
     {
-        return true;
+       if(!COMMANDS.Contains(input))
+		{
+			Console.WriteLine("ERROR: Invalid input. Please try again.");
+			PressEnterContinue();
+			return false;
+		}
+		else
+		{
+			return true;
+		}
     }
 
 
